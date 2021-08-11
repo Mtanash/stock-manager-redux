128ca92738fe04aee8adc149f19d1d9248b61211
Mohamed Tanash <tanashkoo@gmail.com>
2021-08-12 00:24:35 +0200

    organized search result panel file

--- src/components/SearchResultPanel.js
+++ src/components/SearchResultPanel.js
@@ -1,13 +1,22 @@
+// react imports
 import React from "react";
+
+// redux imports
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
 import { setCurrentItem } from "../features/itemSlice";
 import {
   selectSearchResult,
   setSearchPanelIsOpen,
 } from "../features/searchSlice";
 import { selectActiveStock } from "../features/stockSlice";
+
+// react router dom imports
+import { useHistory } from "react-router-dom";
+
+// firebase imports
 import { db } from "./firebase";
+
+// css imports
 import "./SearchResultPanel.css";
 
 function SearchResultPanel() {
